3cd5b9ef475dc3b1b9bdb1e6882a2537645a205b
RoboDoig <erskine94@gmail.com>
2024-09-06 12:55:53 +0100

    Initial commit

---
+++ Bonsai.Emergent/EmergentCapture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emergent;
+
+namespace Bonsai.Emergent
+{
+    public class EmergentCapture : Source<int>
+    {
+
+
+        public override IObservable<int> Generate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
