a9793ff23fab89b7be20274f676d04d2ebf741ff
glopesdev <g.lopes@neurogears.org>
2025-11-11 18:08:05 +0000

    Refactor API surface and include image metadata (#4)
    * Add EmergentDataFrame and ImageMetadata
    * PixelFormat names must be preserved for driver compatibility
    * Simplify acquisition loop and precache properties
    * Allow configuring maximum number of preallocated frames
    * Ensure runtime specific library is referenced
    * Fallback to first detected camera
    * Add XML prefix and namespace icon

---
+++ src/Bonsai.Emergent/ImageMetadata.cs
@@ -0,0 +1,39 @@
+﻿using Emergent;
+
+namespace Bonsai.Emergent
+{
+    /// <summary>
+    /// Represents additional information about an image decoded from an Emergent Vision camera.
+    /// </summary>
+    public class ImageMetadata
+    {
+        internal ImageMetadata(CEmergentFrameDotNet frame)
+        {
+            NanoSeconds = frame.NanoSeconds;
+            Timestamp = frame.Timestamp;
+            FrameId = frame.FrameId;
+            PixelFormat = (PixelFormat)frame.PixelFormat;
+        }
+
+        /// <summary>
+        /// Gets the precise timestamp in nanoseconds for frames using Myri sync NIC and IRIGB.
+        /// </summary>
+        public ulong NanoSeconds { get; }
+
+        /// <summary>
+        /// Gets the GigE Vision timestamp. If the camera is in precision time protocol (PTP) mode,
+        /// this is the PTP clock of the camera.
+        /// </summary>
+        public ulong Timestamp { get; }
+
+        /// <summary>
+        /// Gets the 16-bit block identifier from the GigE Vision Streaming Protocol.
+        /// </summary>
+        public ushort FrameId { get; }
+
+        /// <summary>
+        /// Gets the pixel format of the decoded image data.
+        /// </summary>
+        public PixelFormat PixelFormat { get; }
+    }
+}
--- src/Bonsai.Emergent/PixelFormat.cs
+++ src/Bonsai.Emergent/PixelFormat.cs
@@ -135,32 +135,32 @@ namespace Bonsai.Emergent
         /// <summary>
         /// Color red, green, blue 8-bit channels per pixel.
         /// </summary>
-        Rgb8 = CEmergentFrameDotNet.EPixelFormat.EGVSP_PIX_RGB8,
+        RGB8 = CEmergentFrameDotNet.EPixelFormat.EGVSP_PIX_RGB8,
 
         /// <summary>
         /// Color red, green, blue 10-bit channels per pixel.
         /// </summary>
-        Rgb10 = CEmergentFrameDotNet.EPixelFormat.EGVSP_PIX_RGB10,
+        RGB10 = CEmergentFrameDotNet.EPixelFormat.EGVSP_PIX_RGB10,
 
         /// <summary>
         /// Color red, green, blue 12-bit channels per pixel.
         /// </summary>
-        Rgb12 = CEmergentFrameDotNet.EPixelFormat.EGVSP_PIX_RGB12,
+        RGB12 = CEmergentFrameDotNet.EPixelFormat.EGVSP_PIX_RGB12,
 
         /// <summary>
         /// Color blue, green, red 8-bit channels per pixel.
         /// </summary>
-        Bgr8 = CEmergentFrameDotNet.EPixelFormat.EGVSP_PIX_BGR8,
+        BGR8 = CEmergentFrameDotNet.EPixelFormat.EGVSP_PIX_BGR8,
 
         /// <summary>
         /// Color blue, green, red 10-bit channels per pixel.
         /// </summary>
-        Bgr10 = CEmergentFrameDotNet.EPixelFormat.EGVSP_PIX_BGR10,
+        BGR10 = CEmergentFrameDotNet.EPixelFormat.EGVSP_PIX_BGR10,
 
         /// <summary>
         /// Color blue, green, red 12-bit channels per pixel.
         /// </summary>
-        Bgr12 = CEmergentFrameDotNet.EPixelFormat.EGVSP_PIX_BGR12,
+        BGR12 = CEmergentFrameDotNet.EPixelFormat.EGVSP_PIX_BGR12,
 
         /// <summary>
         /// YUV 411 pixel format packed (no padding bits).
@@ -169,7 +169,7 @@ namespace Bonsai.Emergent
         /// For every four pixels, the Y component is fully sampled for all, but the U and V
         /// components are sampled once and shared across these four pixels.
         /// </remarks>
-        Yuv411Packed = CEmergentFrameDotNet.EPixelFormat.EGVSP_PIX_YUV411_PACKED,
+        YUV411Packed = CEmergentFrameDotNet.EPixelFormat.EGVSP_PIX_YUV411_PACKED,
 
         /// <summary>
         /// YUV 422 pixel format packed (no padding bits).
@@ -178,12 +178,12 @@ namespace Bonsai.Emergent
         /// For every two pixels, the Y component is fully sampled for both, but the U and V
         /// components are sampled once and shared across these two pixels.
         /// </remarks>
-        Yuv422Packed = CEmergentFrameDotNet.EPixelFormat.EGVSP_PIX_YUV422_PACKED,
+        YUV422Packed = CEmergentFrameDotNet.EPixelFormat.EGVSP_PIX_YUV422_PACKED,
 
         /// <summary>
         /// YUV 444 pixel format packed (no padding bits). The Y, U, and V components are fully
         /// sampled for every pixel.
         /// </summary>
-        Yuv444Packed = CEmergentFrameDotNet.EPixelFormat.EGVSP_PIX_YUV444_PACKED
+        YUV444Packed = CEmergentFrameDotNet.EPixelFormat.EGVSP_PIX_YUV444_PACKED
     }
 }
